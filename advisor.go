fa08d9c9741c0d714cba5c09c2a52b3ce5eccc6b
Jhouboze <e50310@gmail.com>
2020-12-31 00:39:38 +0800

    init

--- advisor.go
+++ advisor.go
@@ -1,7 +1,10 @@
 package botrade
 
+// Advisor 可取得各種訊息與交易功能
 type Advisor struct {}
 
+// 取得K棒高點
+// shift: 第幾根K棒
 func (b *Advisor) High(shift int) float64 {
 	return float64(shift)+1.1
 }
--- botrade.go
+++ botrade.go
@@ -1,5 +1,6 @@
 package botrade
 
+// Strategy 策略需實作的方法
 type Strategy interface {
 	SetAdvisor(*Advisor)
 	OnInit()
@@ -7,11 +8,13 @@ type Strategy interface {
 	OnTick()
 }
 
+// Bot 交易機器人
 type Bot struct {
 	apiKey string
 	secretKey string
 }
 
+// NewBot 建立新Bot
 func NewBot(apiKey, secretKey string) *Bot {
 	return &Bot{
 		apiKey: apiKey,
@@ -19,9 +22,15 @@ func NewBot(apiKey, secretKey string) *Bot {
 	}
 }
 
+// Trading 開始交易
 func (b *Bot) Trading(s Strategy) {
 	s.SetAdvisor(&Advisor{})
 	s.OnInit()
 	s.OnTick()
 	s.OnDeinit()
+}
+
+// Testing 開始回測
+func (b *Bot) Testing(s Strategy) {
+
 }
